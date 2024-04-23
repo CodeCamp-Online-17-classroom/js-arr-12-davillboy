c1fe951a031313e9b12d6c4a596d9498fe3af9b6
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 18:30:22 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+        const cars = ['volvo', 'toyota', 'honda', 'ford'];
+        const hondaIndex = cars.indexOf('honda');
+        
+        if (hondaIndex !== -1) {
+        cars.splice(hondaIndex + 1, 0, 'mazda', 'bmw', 'nissan');
+        }
+        
+        console.log(cars); // ["volvo", "toyota", "honda", "mazda", "bmw", "nissan", "ford"]
